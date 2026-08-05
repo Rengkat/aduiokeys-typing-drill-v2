c83979d8326f239f40a5ba8e7c652978eba1ee92
Rengkat <successinmotion11@gmail.com>
2026-08-05 07:37:39 +0100

    update: remove .next

--- .next/dev/static/chunks/app_stage_[stageId]_page_tsx_1ihuo23._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_1208ib9._.js","static/chunks/node_modules_0bkbx4w._.js"],
-    source: "dynamic"
-});
